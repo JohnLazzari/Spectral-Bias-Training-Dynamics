23101203d9a78bf692dc4aa1da512f50185319a7
John Lazzari <lazzarijohn2@gmail.com>
2023-03-28 19:30:54 -0400

    changes

--- scripts/hamming_image.py
+++ scripts/hamming_image.py
@@ -26,7 +26,9 @@ class Net(nn.Module):
         self.hidden = hidden
         self.l1 = nn.Linear(input_dim, hidden)
         self.l2 = nn.Linear(hidden, hidden)
-        self.l3 = nn.Linear(hidden, 3)
+        self.l3 = nn.Linear(hidden, hidden)
+        self.l4 = nn.Linear(hidden, hidden)
+        self.l5 = nn.Linear(hidden, 3)
         self.relu = nn.ReLU()
 
     def forward(self, x, act=False):
@@ -41,13 +43,43 @@ class Net(nn.Module):
         if act:
             act_2 = out_2
 
-        out_3 = self.l3(out_2)
+        out_3 = self.relu(self.l3(out_2))
 
         if act:
-            pattern = torch.cat((act_1, act_2), dim=0).squeeze()
+            act_3 = out_3
+
+        out_4 = self.relu(self.l4(out_3))
+
+        if act:
+            act_4 = out_4
+
+        out_5 = self.l5(out_4)
+
+        if act:
+            pattern = torch.cat((act_1, act_2, act_3, act_4), dim=0).squeeze()
             return pattern
 
-        return out_3
+        return out_5
+
+def set_positive_values_to_one(tensor):
+    # Create a mask that is 1 for positive values and 0 for non-positive values
+    mask = tensor.gt(0.0)
+    # Set all positive values to 1 using the mask
+    tensor[mask] = 1
+    return tensor
+
+def compute_hamming(model, x_0, x_1):
+
+    # hamming distance
+    with torch.no_grad():
+        pattern_1 = model(x_0, act=True)
+        pattern_1 = set_positive_values_to_one(pattern_1)
+        pattern_2 = model(x_1, act=True)
+        pattern_2 = set_positive_values_to_one(pattern_2)
+
+    hamming = torch.sum(torch.abs(pattern_1-pattern_2))
+
+    return hamming.item()
 
 def compute_confusion(model, optim, x_0, x_1, y_0, y_1):
     # Get confusion between the gradients for both inputs
@@ -61,13 +93,8 @@ def compute_confusion(model, optim, x_0, x_1, y_0, y_1):
     loss_1 = criterion(output, y_0)
     loss_1.backward()
 
-    '''
     grad_1 = torch.cat([torch.flatten(model.l1.weight.grad), torch.flatten(model.l2.weight.grad), torch.flatten(model.l3.weight.grad), torch.flatten(model.l4.weight.grad), torch.flatten(model.l5.weight.grad),
                         torch.flatten(model.l1.bias.grad), torch.flatten(model.l2.bias.grad), torch.flatten(model.l3.bias.grad), torch.flatten(model.l4.bias.grad), torch.flatten(model.l5.bias.grad),
-                        ])
-    '''
-    grad_1 = torch.cat([torch.flatten(model.l1.weight.grad), torch.flatten(model.l2.weight.grad), torch.flatten(model.l3.weight.grad), 
-                        torch.flatten(model.l1.bias.grad), torch.flatten(model.l2.bias.grad), torch.flatten(model.l3.bias.grad)
                         ])
 
     for param in model.parameters():
@@ -76,13 +103,8 @@ def compute_confusion(model, optim, x_0, x_1, y_0, y_1):
     output = model(x_1)
     loss_2 = criterion(output, y_1)
     loss_2.backward()
-    '''
     grad_2 = torch.cat([torch.flatten(model.l1.weight.grad), torch.flatten(model.l2.weight.grad), torch.flatten(model.l3.weight.grad), torch.flatten(model.l4.weight.grad), torch.flatten(model.l5.weight.grad),
                         torch.flatten(model.l1.bias.grad), torch.flatten(model.l2.bias.grad), torch.flatten(model.l3.bias.grad), torch.flatten(model.l4.bias.grad), torch.flatten(model.l5.bias.grad),
-                        ])
-    '''
-    grad_2 = torch.cat([torch.flatten(model.l1.weight.grad), torch.flatten(model.l2.weight.grad), torch.flatten(model.l3.weight.grad), 
-                        torch.flatten(model.l1.bias.grad), torch.flatten(model.l2.bias.grad), torch.flatten(model.l3.bias.grad)
                         ])
 
     # get inner products of gradients
@@ -94,9 +116,10 @@ def compute_confusion(model, optim, x_0, x_1, y_0, y_1):
 
     return confusion.item()
 
-def confusion_local(model, optim, inp_batch, inp_target, iterations):
+def hamming_within_regions(model, optim, inp_batch, inp_target, iterations, comp_hamming=True, comp_confusion=True):
 
-    print('Getting confusion within local regions...')
+    print('Starting hamming within local regions...')
+    hamming_local = []
     confusion_local = []
     # reshape the inputs to be in image format again
     shape = inp_batch.shape
@@ -112,22 +135,28 @@ def confusion_local(model, optim, inp_batch, inp_target, iterations):
         patch = torch.flatten(inp_batch[rand_x-25:rand_x+25, rand_y-25:rand_y+25, :], start_dim=0, end_dim=1)
         patch_target = torch.flatten(inp_target[rand_x-25:rand_x+25, rand_y-25:rand_y+25, :], start_dim=0, end_dim=1)
 
-        for j in range(500):
+        for j in range(250):
 
             ind1 = np.random.randint(0, 2499)
             ind2 = np.random.randint(0, 2499)
 
-            confusion = compute_confusion(model, optim, patch[ind1], patch[ind2], patch_target[ind1], patch_target[ind2])
-            confusion_local.append(confusion)
+            if comp_hamming:
+                hamming = compute_hamming(model, patch[ind1], patch[ind2])
+                hamming_local.append(hamming)
+            if comp_confusion:
+                confusion = compute_confusion(model, optim, patch[ind1], patch[ind2], patch_target[ind1], patch_target[ind2])
+                confusion_local.append(confusion)
 
-    return confusion_local
+    return hamming_local, confusion_local
 
-def confusion_global(model, optim, inp_batch, inp_target, iterations):
+def hamming_between_regions(model, optim, inp_batch, inp_target, iterations, comp_hamming=True, comp_confusion=True):
 
-    print('Getting confusion across input space...')
+    print('Starting hamming across input space...')
+    hamming_between = []
     confusion_between = []
     # reshape the inputs to be in image format again
     shape = inp_batch.shape
+    loss = []
     inp_batch = torch.reshape(inp_batch, [512, 512, shape[-1]])
     inp_target = torch.reshape(inp_target, [512, 512, 3])
 
@@ -153,10 +182,97 @@ def confusion_global(model, optim, inp_batch, inp_target, iterations):
         point_2, point_2_target = inp_batch[rand_x_2, rand_y_2, :], inp_target[rand_x_2, rand_y_2, :]
         point_2, point_2_target = point_2.squeeze(), point_2_target.squeeze()
 
-        confusion = compute_confusion(model, optim, point_1, point_2, point_1_target, point_2_target)
-        confusion_between.append(confusion)
+        if comp_hamming:
+            hamming = compute_hamming(model, point_1, point_2)
+            hamming_between.append(hamming)
+        if comp_confusion:    
+            confusion = compute_confusion(model, optim, point_1, point_2, point_1_target, point_2_target)
+            confusion_between.append(confusion)
 
-    return confusion_between
+    return hamming_between, confusion_between
+
+def get_activation_regions(model, input):
+    with torch.no_grad():
+        patterns = []
+        # get patterns
+        for i, pixel in enumerate(input):
+            pixel = pixel.squeeze()
+            act_pattern = model(pixel, act=True)
+            act_pattern = set_positive_values_to_one(act_pattern)
+            patterns.append(list(act_pattern.detach().cpu().numpy()))
+        # get the amount of unique patterns
+        unique_patterns = []
+        for pattern in patterns:
+            if pattern not in unique_patterns:
+                unique_patterns.append(pattern)
+    return len(unique_patterns), unique_patterns, patterns
+
+def plot_patterns(patterns, all_patterns):
+
+    dict_patterns = {}
+    # only plotting for inputs, not for counting regions
+    colors = np.zeros(512*512)
+    random.shuffle(patterns)
+    for i, pattern in enumerate(patterns):
+        # assign each position a color
+        dict_patterns[tuple(pattern)] = i
+    for i, pattern in enumerate(all_patterns):
+        colors[i] = dict_patterns[tuple(pattern)]
+    
+    colors = np.reshape(colors, [512, 512])
+    plt.pcolormesh(colors, cmap='Spectral')
+    plt.colorbar()
+    plt.show()
+
+def plot_first_3_neurons(all_patterns):
+
+    all_patterns = np.array(all_patterns)
+    all_patterns = np.reshape(all_patterns, [512, 512, -1])
+
+    # get neurons that arent all dead or activated to show hyperplane
+    neuron_list = []
+
+    # get random neurons to demonstrate hyperplane arrangement
+    first_neuron = all_patterns[:, :, 0]
+    second_neuron = all_patterns[:, :, 9]
+    third_neuron = all_patterns[:, :, 100]
+
+    fig = plt.figure(figsize=(12, 4))
+
+    ax1 = fig.add_subplot(131)
+    ax2 = fig.add_subplot(132)
+    ax3 = fig.add_subplot(133)
+
+    pcm1 = ax1.pcolormesh(first_neuron, cmap='Spectral')
+    pcm2 = ax2.pcolormesh(second_neuron, cmap='Spectral')
+    pcm3 = ax3.pcolormesh(third_neuron, cmap='Spectral')
+
+    ax1.tick_params(left = False, right = False , labelleft = False ,
+            labelbottom = False, bottom = False)
+    ax2.tick_params(left = False, right = False , labelleft = False ,
+            labelbottom = False, bottom = False)
+    ax3.tick_params(left = False, right = False , labelleft = False ,
+            labelbottom = False, bottom = False)
+
+    plt.show()
+
+def get_zero_neurons(model, input):
+    # get patterns
+    # Change this value to the total number of neurons in the network
+    zero_neurons = np.zeros([256])
+    for i, pixel in enumerate(input):
+        pixel = pixel.squeeze()
+        act_pattern = model(pixel, act=True)
+        act_pattern = set_positive_values_to_one(act_pattern)
+        act_pattern = act_pattern.detach().cpu().numpy()
+        for j in range(act_pattern.shape[0]):
+            if act_pattern[j] == 0.:
+                zero_neurons[j] += 1
+    dead_count = 0
+    for item in zero_neurons:
+        if item == 512*512:
+            dead_count += 1
+    return dead_count
 
 def get_data(image, encoding, L=10, batch_size=2048, negative=False, shuffle=True):
 
@@ -206,15 +322,45 @@ def train(model, optim, criterion, im, encoding, L, args):
     train_inp_batch, train_inp_target = get_data(image=im, encoding=encoding, L=L, batch_size=args.batch_size, negative=args.negative)
     inp_batch, inp_target = get_data(image=im, encoding=encoding, L=L, batch_size=1, shuffle=False, negative=args.negative)
 
+    # lists containing values for various experiments
+    param_norms = []
+    num_patterns = []
+
+    hamming_in_region = []
     confusion_in_region = []
+    hamming_between_region = []
     confusion_between_region = []
+
+    min_confusion_in_region = []
+    min_confusion_between_region = []
+
+    mean_hamming_within = []
+    std_hamming_within = []
+    mean_hamming_between = []
+    std_hamming_between = []
+
+    layer_1_norms = []
+    layer_3_norms = []
+    layer_2_norms = []
+
+    zero_neuron_plot = []
 
     losses = []
 
     # Start the training loop
     for epoch in range(args.epochs):
 
+        dead_neuron_count = 0
         running_loss = 0
+
+        # get the number of dead neurons, start at initialization
+        if args.dead_neurons:
+            if epoch % 100 == 0:
+                with torch.no_grad():
+                    print('Counting dead ReLU neurons...')
+                    dead = get_zero_neurons(model, inp_batch)
+                    print('Number dead neurons at epoch {}: {}'.format(epoch, dead))
+                    zero_neuron_plot.append(dead)
 
         # Train the model for one epoch
         for i, pixel in enumerate(train_inp_batch):
@@ -235,22 +381,72 @@ def train(model, optim, criterion, im, encoding, L, args):
         losses.append(epoch_loss)
 
         # This gets confusion at the end of training, specify the number of epochs
-        if epoch > args.epochs-2:
-            confusion_in_region = confusion_local(model, optim, inp_batch, inp_target, 150)
-            confusion_between_region = confusion_global(model, optim, inp_batch, inp_target, 50000)
+        if args.confusion_in_region:
+            if epoch > args.epochs-2:
+                # 100
+                _, confusion_in_region = hamming_within_regions(model, optim, inp_batch, inp_target, 100, comp_hamming=False)
 
-    return confusion_in_region, confusion_between_region
+        if args.confusion_between_region:
+            if epoch > args.epochs-2:
+                # 25000
+                _, confusion_between_region = hamming_between_regions(model, optim, inp_batch, inp_target, 25000, comp_hamming=False)
+
+        # mean hamming distance during training
+        if args.mean_hamming_in_region:
+            if epoch % 250 == 0:
+                hamming_in_region, confusion_in = hamming_within_regions(model, optim, inp_batch, inp_target, 100, comp_confusion=False)
+                mean_hamming_within.append(np.mean(np.array(hamming_in_region)))
+                std_hamming_within.append(np.std(np.array(hamming_in_region)))
+
+        if args.mean_hamming_between_region:
+            if epoch % 250 == 0:
+                hamming_between_region, confusion_between = hamming_between_regions(model, optim, inp_batch, inp_target, 50000, comp_confusion=False)
+                mean_hamming_between.append(np.mean(np.array(hamming_between_region)))
+                std_hamming_between.append(np.std(np.array(hamming_between_region)))
+
+        # Calculate the spectral norm of the weight matrices for param norms
+        if args.param_norms:
+            with torch.no_grad():
+                # Go through each layer and multiply the norms
+                U, S, V = torch.linalg.svd(model.l1.weight)
+                norm_1 = max(S)
+                layer_1_norms.append(norm_1.item())
+                U, S, V = torch.linalg.svd(model.l2.weight)
+                norm_2 = max(S)
+                layer_2_norms.append(norm_2.item())
+                U, S, V = torch.linalg.svd(model.l3.weight)
+                norm_3 = max(S)
+                layer_3_norms.append(norm_3.item())
+                total_norm = norm_1 * norm_2 * norm_3
+                param_norms.append(total_norm.item())
+
+        # get activation regions during training for plot
+        if args.act_patterns:
+            # Get num regions for raw_xy
+            if epoch % 100 == 0:
+                print('Counting activation patterns...')
+                raw_regions, unique_patterns, all_patterns = get_activation_regions(model, inp_batch)
+                print('number of unique activation regions raw_xy: {}'.format(raw_regions))
+                num_patterns.append(raw_regions)
+
+    return param_norms, num_patterns, layer_1_norms, layer_2_norms, layer_3_norms, zero_neuron_plot, losses, inp_batch, inp_target, confusion_in_region, confusion_between_region, mean_hamming_within, mean_hamming_between
 
 def main():
 
     parser = argparse.ArgumentParser(description='Blah.')
-    parser.add_argument('--neurons', type=int, default=512, help='Number of neurons per layer')
+    parser.add_argument('--neurons', type=int, default=256, help='Number of neurons per layer')
     parser.add_argument('--epochs', type=int, default=5000, help='Number of epochs')
     parser.add_argument('--batch_size', type=int, default=4096, help='make a training and testing set')
     parser.add_argument('--print_loss', type=bool, default=True, help='print training loss')
+    parser.add_argument('--param_norms', type=bool, default=False, help='plot param norms')
+    parser.add_argument('--dead_neurons', type=bool, default=False, help='track dead neurons')
+    parser.add_argument('--mean_hamming_in_region', type=bool, default=False, help='doing line count')
+    parser.add_argument('--mean_hamming_between_region', type=bool, default=False, help='doing line count')
     parser.add_argument('--confusion_in_region', type=bool, default=False, help='doing line count')
     parser.add_argument('--confusion_between_region', type=bool, default=False, help='doing line count')
+    parser.add_argument('--act_patterns', type=bool, default=False, help='check number of unique activation patterns')
     parser.add_argument('--negative', type=bool, default=False, help='-1 to 1')
+    parser.add_argument('--visualize_regions', type=bool, default=False, help='plot the losses')
     parser.add_argument('--train_encoding', action='store_false', default=True, help='train positional encoding')
     parser.add_argument('--train_coordinates', action='store_false', default=True, help='train coordinates')
 
@@ -258,7 +454,7 @@ def main():
 
     # change image to any in image_demonstration
     test_data = np.load('test_data_div2k.npy')
-    test_data = test_data[:1]
+    test_data = test_data[:5]
 
     L_vals = [4, 8, 16]
     # lists for all confusion for each image
@@ -284,7 +480,7 @@ def main():
 
         if args.train_coordinates:
             print("Beginning Raw XY Training...")
-            confusion_within_xy, confusion_between_xy = train(model_raw, optim_raw, criterion, im, 'raw_xy', 0, args)
+            xy_param_norms, xy_num_patterns, xy_layer_1_norms, xy_layer_2_norms, xy_layer_3_norms, xy_zero_neuron_plot, xy_losses, inp_batch, inp_target, confusion_within_xy, confusion_between_xy, mean_hamming_within_xy, mean_hamming_between_xy = train(model_raw, optim_raw, criterion, im, 'raw_xy', 0, args)
         
         averaged_global_confusion_xy.append(confusion_between_xy)
         averaged_local_confusion_xy.append(confusion_within_xy)
@@ -300,7 +496,7 @@ def main():
                 optim_pe = torch.optim.Adam(model_pe.parameters(), lr=.001)
                 criterion = nn.MSELoss()
 
-                confusion_within_pe, confusion_between_pe = train(model_pe, optim_pe, criterion, im, 'sin_cos', l, args)
+                pe_param_norms, pe_num_patterns, pe_layer_1_norms, pe_layer_2_norms, pe_layer_3_norms, pe_zero_neuron_plot, pe_losses, inp_batch, inp_target, confusion_within_pe, confusion_between_pe, mean_hamming_within_pe, mean_hamming_between_pe = train(model_pe, optim_pe, criterion, im, 'sin_cos', l, args)
 
                 averaged_global_confusion_pe[f'{l}_val'].append(confusion_between_pe)
                 averaged_local_confusion_pe[f'{l}_val'].append(confusion_within_pe)
